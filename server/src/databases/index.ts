a5c4e6df61e77af9752402bfce10e0a2ed3884da
Brixter Porras <brixterporras@gmail.com>
2022-09-03 21:42:47 +0800

    Initialize server with mysql

---
+++ server/src/databases/index.ts
@@ -0,0 +1,17 @@
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from '@config';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '../**/*.migration{.ts,.js}')],
+  subscribers: [join(__dirname, '../**/*.subscriber{.ts,.js}')],
+});
