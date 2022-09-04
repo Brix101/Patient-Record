c29a7cca98feb0f3485eb7ff68a3391e371eca0d
Brixter Porras <brixterporras@gmail.com>
2022-09-04 21:48:02 +0800

    Added Mui Dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
+import { LinearProgress } from "@mui/material";
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/admin/Dashboard";
 import Main from "./layout/Main";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -15,23 +17,25 @@ function App() {
   });
 
   if (isError) return <div>Something went wrong</div>;
+  if (isLoading) return <LinearProgress color="primary" />;
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-        {/* <Route path="dashboard" element={<Admin />}>
-          <Route index element={<Dashboard />} />
-          <Route path="account" element={<Account />} />
-          <Route path="users" element={<Users />} />
-        </Route> */}
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </BrowserRouter>
+    <Dashboard />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Main />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="about" element={<About />} />
+    //       <Route path="contact" element={<Contact />} />
+    //     </Route>
+    //     {/* <Route path="dashboard" element={<Admin />}>
+    //       <Route index element={<Dashboard />} />
+    //       <Route path="account" element={<Account />} />
+    //       <Route path="users" element={<Users />} />
+    //     </Route> */}
+    //     <Route path="*" element={<Page404 />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
