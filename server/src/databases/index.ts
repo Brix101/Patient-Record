2a685506328a404a5dc5538f0ccab20498dd2d3c
Brixter Porras <brixterporras@gmail.com>
2022-09-20 21:32:08 +0800

    Refactor server to Django

--- server/src/databases/index.ts
+++
@@ -1,9 +0,0 @@
-import { DB_DATABASE, DB_HOST, DB_PORT } from '@config';
-
-export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-};
