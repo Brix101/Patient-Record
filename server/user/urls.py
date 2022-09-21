0dd3363dad4dcbd9b6e64bba507b5c06a9867950
Brixter Porras <brixterporras@gmail.com>
2022-09-21 22:23:00 +0800

    Added Logout

--- server/user/urls.py
+++ server/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.Users.as_view()),
     path('login/', views.LoginView.as_view(), name='auth_login'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('logout/', views.LogOutView.as_view(), name='auth_logout'),
 ]
--- server/user/views.py
+++ server/user/views.py
@@ -1,24 +1,11 @@
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import RegisterSerializer, LoginSerializer
 from rest_framework import generics, status
 from .models import User
-from rest_framework.permissions import AllowAny
-from django.contrib.auth import login
-
-
-class Users(APIView):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, format=None):
-        content = {
-            'user': str(request.user),  # `django.contrib.auth.User` instance.
-            'auth': str(request.auth),  # None
-        }
-        return Response(content)
+from django.contrib.auth import login,logout
 
 
 class RegisterView(generics.CreateAPIView):
@@ -37,4 +24,12 @@ class LoginView(APIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
+
+class LogOutView(APIView):
+    # This view should be accessible also for unauthenticated users.
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        logout(request)
         return Response(None, status=status.HTTP_202_ACCEPTED)
