c261573c0970fd1e7796c35bf3c3e43de10f334b
Brixter Porras <brixterporras@gmail.com>
2022-09-21 22:08:01 +0800

    Added User Registration and Login

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Users
+from . import views
 
 urlpatterns = [
-    path('users/', Users.as_view()),
+    path('users/', views.Users.as_view()),
+    path('login/', views.LoginView.as_view(), name='auth_login'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
 ]
