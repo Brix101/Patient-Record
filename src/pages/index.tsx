fa7811da8346d4ea83c7edd6dde1e03ff8261e76
Brixter Porras <brixterporras@gmail.com>
2022-12-08 22:06:44 +0800

    chore: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ const Home: NextPage = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white"
+                  className="hidden py-2 pr-4 pl-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white"
                   aria-current="page"
                 >
                   Home
@@ -68,7 +68,7 @@ const Home: NextPage = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pr-4 pl-3 text-grey-700 rounded hover:bg-green-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-green-400 md:dark:hover:text-white dark:hover:bg-green-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="hidden py-2 pr-4 pl-3 text-grey-700 rounded hover:bg-green-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-green-400 md:dark:hover:text-white dark:hover:bg-green-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   About
                 </a>
